08ad2c811b77fb809a54a06588516998b5cea4a4
maxim486 <91800950+maxim486@users.noreply.github.com>
2022-04-23 01:56:51 -0400

    Update acf-sync.bat

--- acf-sync.bat
+++ acf-sync.bat
@@ -3,23 +3,17 @@
 @echo off 
 setlocal enableextensions 
 
-:: Need to be within the \steamapps directory for findstr to work
-:: 	there is less clutter in \SteamLibrary so it's easier to launch from here
 cd steamapps
 mkdir acf-tmp
 move *.acf acf-tmp\ >NUL
 
 :: Get Game Directory Names
 for /d %%D in (.\common\*) do (
-	REM echo %%~nxD
 	:: Find matching acf file that contains game name and copy to ..\tmp
 	for /f "tokens=*" %%a in ('findstr /i /m /c:"%%~nxD" acf-tmp\*.acf') do (
-		REM echo %%a
 		copy %%a . >NUL
 	)
 )
-
 rmdir /S /Q acf-tmp
 
 endlocal
-REM pause
